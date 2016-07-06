9a1d7923d008c74c19d265062a55b4c50b373812
kpaul808 <kainoap808@gmail.com>
2016-07-06 11:54:39 -1000

    added more js, completed entire alphabet

--- js/index.js
+++ js/index.js
@@ -12,6 +12,82 @@ SubmittedCharacter.addEventListener('submit', function(event) {
 function test(){
 	document.getElementById("images").innerHTML = Character
 	if (Character == "A" || Character == "a") {
-		document.getElementById("testC").innerHTML = "<img src= assets/SignLanguage/A.png>"
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/A.png>"
 	}
+	if (Character == "B" || Character == "b"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/B.png>"
+	}
+	if (Character == "C" || Character == "c"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/C.png>"
+	}
+	if (Character == "D" || Character == "d"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/D.png>"
+	}
+	if (Character == "E" || Character == "e"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/E.png>"
+	}
+	if (Character == "F" || Character == "f"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/F.png>"
+	}
+	if (Character == "G" || Character == "g"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/G.png>"
+	}
+	if (Character == "H" || Character == "h"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/H.png>"
+	}
+	if (Character == "I" || Character == "i"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/I.png>"
+	}
+	if (Character == "J" || Character == "j"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/J.png>"
+	}
+	if (Character == "K" || Character == "k"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/K.png>"
+	}
+	if (Character == "L" || Character == "l"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/L.png>"
+	}
+	if (Character == "M" || Character == "m"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/M.png>"
+	}
+	if (Character == "N" || Character == "n"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/N.png>"
+	}
+	if (Character == "O" || Character == "o"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/O.png>"
+	}
+	if (Character == "P" || Character == "p"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/P.png>"
+	}
+	if (Character == "Q" || Character == "q"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/Q.png>"
+	}
+	if (Character == "R" || Character == "r"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/R.png>"
+	}
+	if (Character == "S" || Character == "s"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/S.png>"
+	}
+	if (Character == "T" || Character == "t"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/T.png>"
+	}
+	if (Character == "U" || Character == "u"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/U.png>"
+	}
+	if (Character == "V" || Character == "v"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/V.png>"
+	}
+	if (Character == "W" || Character == "w"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/W.png>"
+	}
+	if (Character == "X" || Character == "x"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/X.png>"
+	}
+	if (Character == "Y" || Character == "y"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/Y.png>"
+	}
+	if (Character == "Z" || Character == "z"){
+		document.getElementById("images").innerHTML = "<img src= assets/SignLanguage/Z.png>"
+	}
+	if (Character.length -- 1 > 1) {}
 }
